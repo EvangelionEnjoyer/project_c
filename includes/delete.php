c5d9d212ff70330738a188cbfb1035e2d4bbcdd9
LucNtw <101431449+LucNtw@users.noreply.github.com>
2024-03-01 08:15:24 +0100

    Update delete.php
    delete Schnittstelle

--- includes/delete.php
+++ includes/delete.php
@@ -3,7 +3,23 @@ if(!isset($_GET['functionDelete'])){
     echo "Error";
     exit;
 }
+
+switch($_GET['functionDelete']){
+    case 'deleteUser': 
+        echo "User deleted";
+    exit;
+
+    case 'delete':
+        echo "deleted":
+    exit;
+
+}
+function deleteUser(){
+    
+}
+
 function delete(){
     
 }
+
 ?>
