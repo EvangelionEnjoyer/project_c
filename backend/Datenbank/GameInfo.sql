b4cdb3fc43f4e113036f8c02d2c9539e0883e5ba
LucNtw <101431449+LucNtw@users.noreply.github.com>
2024-03-06 09:05:05 +0100

    Datenbank
    Verbesserung der Datenbank

--- backend/Datenbank/GameInfo.sql
+++ backend/Datenbank/GameInfo.sql
@@ -1,16 +1,26 @@
-CREATE TABLE tblGameInfo{
-    idGameId INT, 
+CREATE TABLE tblGameInfo (
+    idGameId INT PRIMARY KEY AUTO_INCREMENT, 
     dtTitel VARCHAR(50),
     dtGenre VARCHAR(30),
     dtVeröffentlichungsdatum DATE,
-    fiPubId,
+    dtPGI VARCHAR(5),
+    dtReleasedate DATE,
+    fiPubId INT,
     FOREIGN KEY(fiPubId) REFERENCES tblPublisher(idPubId)
-};
+);
 
-CREATE TABLE tblPublisher{
-    idPubId INT,
+CREATE TABLE tblPublisher (
+    idPubId INT PRIMARY KEY AUTO_INCREMENT,
     dtName VARCHAR(40),
     dtGrundjahr INT,
     dtHauptsitz VARCHAR(50),
     dtWebseite VARCHAR(255)
-};
+);
+
+CREATE TABLE USER (
+    IdUser INT PRIMARY KEY AUTO_INCREMENT,
+    dtVorname VARCHAR(30),
+    dtNachname VARCHAR(30),
+    dtFavGame VARCHAR(30),
+    dtPasswort VARCHAR(255)
+);
